25aaee24ab12f6dfe3f433a240403ef06ae1bfaa
umsungjun <99730910+umsungjun@users.noreply.github.com>
2024-10-12 15:37:36 +0900

    Create 11650.js

---
+++ 11650.js
@@ -0,0 +1,30 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+const n = Number(input[0]);
+
+const arr = [];
+
+for (let i = 1; i <= n; i++) {
+  const [x, y] = input[i].split(" ").map(Number);
+
+  arr.push([x, y]);
+}
+
+const compare = (a, b) => {
+  if (a[0] !== b[0]) {
+    return a[0] - b[0];
+  } else {
+    return a[1] - b[1];
+  }
+};
+
+arr.sort(compare);
+
+let answer = "";
+
+for (let point of arr) {
+  answer += `${point[0]} ${point[1]}\n`;
+}
+
+console.log(answer);
