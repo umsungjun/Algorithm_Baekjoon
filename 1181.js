c964eb5a072291c1abe720e601276ff3ee3f9211
umsungjun <umseongjun@naver.com>
2024-10-15 21:10:10 +0900

    Update 1181.js

--- 1181.js
+++ 1181.js
@@ -11,19 +11,7 @@ for (let i = 1; i <= n; i++) {
   }
 }
 
-arr.sort((a, b) => {
-  if (a.length === b.length) {
-    if (a < b) {
-      return -1;
-    } else if (a > b) {
-      return 1;
-    } else {
-      return 0;
-    }
-  } else {
-    return a.length - b.length;
-  }
-});
+arr.sort((a, b) => a[0] - b[0]);
 
 let answer = "";
 
