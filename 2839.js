fd4b78c0bfb1d72f192780aa4b3deb8e1623f615
umsungjun <umseongjun@naver.com>
2024-10-25 17:36:41 +0900

    Update 2839.js

--- 2839.js
+++ 2839.js
@@ -3,19 +3,19 @@ let input = fs.readFileSync("/dev/stdin").toString().split("\n");
 
 let num = Number(input[0]); // 18
 let answer = 0;
+let flag = false;
 
-while (num > 3) {
-  if (num - 5 >= 5 && num % 3 !== 0) {
-    num -= 5;
-    answer += 1;
-  } else {
-    num -= 3;
-    answer += 1;
+while (num >= 0) {
+  if (num === 0 || num % 5 === 0) {
+    answer += parseInt(num / 5);
+    console.log(answer);
+    flag = true;
+    break;
   }
+  num -= 3;
+  answer += 1;
 }
 
-if (num > 0) {
+if (!flag) {
   console.log(-1);
-} else {
-  console.log(answer);
 }
